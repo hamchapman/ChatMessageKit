871eeba125918500840aa5bc9382c8038e125720
Hamilton Chapman <hamchapman@gmail.com>
2018-11-30 17:13:22 +0000

    Initial Commit

---
+++ ChatMessageKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMessageKit
+//
+//  Created by Hamilton Chapman on 30/11/2018.
+//  Copyright © 2018 Hamilton Chapman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ChatMessageKitTests/ChatMessageKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChatMessageKitTests.swift
+//  ChatMessageKitTests
+//
+//  Created by Hamilton Chapman on 30/11/2018.
+//  Copyright © 2018 Hamilton Chapman. All rights reserved.
+//
+
+import XCTest
+@testable import ChatMessageKit
+
+class ChatMessageKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
